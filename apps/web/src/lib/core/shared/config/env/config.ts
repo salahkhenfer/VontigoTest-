33ab2666d32b1071d6280454c47e8d273df4bfa7
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2023-12-12 21:27:44 +0100

    Initial commit

---
+++ apps/web/src/lib/core/shared/config/env/config.ts
@@ -0,0 +1,48 @@
+import {
+	POSTGRES_HOST,
+	POSTGRES_DATABASE,
+	POSTGRES_PASSWORD,
+	POSTGRES_USER
+} from '$env/static/private';
+
+export const globalConfig = {
+	database: {
+		sqlite: {
+			client: 'sqlite3',
+			connection: {
+				filename: './database/vontigo.db'
+			},
+			useNullAsDefault: true
+		},
+		postgres: {
+			client: 'pg',
+			connection: {
+				host: POSTGRES_HOST,
+				user: POSTGRES_USER,
+				password: POSTGRES_PASSWORD,
+				database: POSTGRES_DATABASE,
+				ssl: false,
+				sslmode: 'verify-full'
+			},
+			pool: { min: 0, max: 7 }
+			// "client": "mysql",
+			// "connection": {
+			// 	"host": "179.61.199.11",
+			// 	"user": "vontigo",
+			// 	"password": "VontigoP@ssw0rd",
+			// 	"database": "vontigo",
+			// 	"charset": "utf8"
+			// }
+		}
+	},
+	paths: {
+		contentPath: 'content/'
+	},
+	logging: {
+		level: 'info',
+		rotation: {
+			enabled: true
+		},
+		transports: ['file', 'stdout']
+	}
+};
